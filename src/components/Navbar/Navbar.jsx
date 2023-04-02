ffad23aec7504fed0695d58e0eecec6da3e9c626
Md. Jahirul Islam <jahirulislam12283@gmail.com>
2023-04-03 04:06:31 +0600

    added simple Navbar and toggler with react & tailwind css

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import Link from '../Link/LInk';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
+
+
+
+
+
+    const routes = [
+        {
+            id: 1,
+            name: 'Homepage',
+            path: '/'
+        },
+        {
+            id: 2,
+            name: 'About Us',
+            path: '/about'
+        },
+        {
+            id: 3,
+            name: 'Services',
+            path: '/services'
+        },
+        {
+            id: 4,
+            name: 'Contact Us',
+            path: '/contact'
+        },
+        {
+            id: 5,
+            name: 'FAQs',
+            path: '/faqs'
+        }
+    ];
+
+
+
+    return (
+        <nav className='bg-purple-400'>
+            <div onClick={() => setOpen(!open)} className='md:hidden'>
+                <span>
+                    {open === true ?
+                        <XMarkIcon className="h-6 w-6 text-blue-500" /> :
+                        <Bars3Icon className="h-6 w-6 text-blue-500" />
+                    }
+                </span>
+
+            </div>
+            <ul className={`md:flex absolute md:static duration-500 pl-8 py-3 rounded-sm bg-purple-400 ${open ? 'top-5' : '-top-36'}`}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+
+            </ul>
+
+
+        </nav>
+    );
+};
+
+export default Navbar;
